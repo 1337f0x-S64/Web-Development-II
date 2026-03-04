3c636a0a52166607533c0db032429184ab1d5ee7
Luis <garciamoraluisale@gmail.com>
2026-03-04 11:24:37 -0600

    Updated weather API practice file

--- weather/server.js
+++ weather/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const { APP_PORT, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, WEATHER_API_KEY, WEATHER_API_URI} = process.env;
-mongoose.connect(`mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`);
+const {
+  APP_PORT,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_NAME,
+  WEATHER_API_KEY,
+  WEATHER_API_URI,
+} = process.env;
+mongoose.connect(
+  `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`,
+);
 
 /**
  * API URI TEMPLATE
@@ -19,38 +28,47 @@ const tempSchema = new Schema(
     timestamps: true,
   },
 );
-const Temp = (mongoose.model("Temp", tempSchema));
+const Temp = mongoose.model("Temp", tempSchema);
 
 const app = express();
 
-async function fetchWeather(cityName)
-{
-  let temp
-  try
-  {
-    const resp = await fetch( `${WEATHER_API_URI}?q=${cityName}&appid=${WEATHER_API_KEY}`)
-    const data = await resp.json()
-    temp = data
-  }catch(e) 
-  {
-      temp = null
-  }finally
-  {
-    return temp
+async function fetchWeather(cityName) {
+  let temp;
+  try {
+    const resp = await fetch(
+      `${WEATHER_API_URI}?q=${cityName}&appid=${WEATHER_API_KEY}`,
+    );
+    const data = await resp.json();
+    temp = data;
+  } catch (e) {
+    temp = null;
+  } finally {
+    return temp;
   }
-  
 }
 
 app.get("/", async (req, res) => {
-
   //query the last temperature in the database
-  const last = await Temp.find().slice('Temp', -1)
+  const last = await Temp.find().slice("Temp", -1);
+  if (last.length == 1) {
+    res.json(last[0]);
+    return;
+  }
 
   //if no temp was found, query the temperature in the weather api
-  const {main:{temp}, name, weather} = await fetchedWeather("Managua")
+  const {
+    main: { temp },
+    name,
+    weather,
+  } = await fetchWeather("Managua");
   //then store the temperature in the database and return that info
-  last = {value: temp, city: name, weather: weather[0], weather_description: weather[0].description}
-  await Temp.create(last)
+  last = {
+    value: temp,
+    city: name,
+    weather: weather[0],
+    weather_description: weather[0].description,
+  };
+  await Temp.create(last);
   //if there is a temp make sure it is as old as 15 minutes
   //if it is older than 15 minutes, re-query the temperature in the weather api
   //then return the value
@@ -59,4 +77,6 @@ app.get("/", async (req, res) => {
 
 app.get("/dashboard", (req, res) => {});
 
-app.listen(APP_PORT, () => console.log(`Server is listening at port: ${APP_PORT}`))
+app.listen(APP_PORT, () =>
+  console.log(`Server is listening at port: ${APP_PORT}`),
+);
