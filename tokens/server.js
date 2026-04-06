e64ea7cee43ce14b50c741f274ade4be94e8e6a9
Luis <garciamoraluisale@gmail.com>
2026-04-06 11:05:50 -0600

    Added new practice files: - JWT practice files - React practice files

--- tokens/server.js
+++ tokens/server.js
@@ -1,72 +1,114 @@
-import express from 'express'
-import Mongoose, { model, Schema } from 'mongoose'
-import bcrypt from 'bcrypt'
-import { expressjwt } from 'express-jwt'
-import jwt from 'jsonwebtoken'
+import express from "express";
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import { expressjwt } from "express-jwt";
 import "dotenv/config.js";
+import jwt from "jsonwebtoken";
+
+mongoose.connect("mongodb://127.0.0.1:27017/jwt");
 
 const userSchema = new Schema(
-    {
-        username: String,
-        password: String,
-    },{
-        timestamps: true
-    },
+  {
+    username: String,
+    password: String,
+  },
+  {
+    timestamps: true,
+  },
 );
 
-Mongoose.connect("mongodb://localhost:27017/jwt")
+const tokenSchema = new Schema(
+  {
+    token: String,
+    blackListed: Boolean,
+  },
+  { timestamps: true },
+);
 
-const User = Mongoose.model("User", userSchema)
+const Token = mongoose.model("Token", tokenSchema);
 
-const app = express()
+const User = mongoose.model("User", userSchema);
 
-app.use(express.json())
-app.use(express.urlencoded())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-    let user = await User.findOne({username}).exec()
+  const { username, password } = req.body;
+  let user = await User.findOne({ username }).exec();
 
-    if(user){
-        return res.status(400).json({error: "sorry :c"})
-    }
+  if (user) {
+    return res.status(400).json({ error: "sorry :c" });
+  }
+  const hashed = bcrypt.hashSync(password, 10);
+  user = await User.create({ username, password: hashed });
 
-    const hashed = bcrypt.hashSync(password, 10);
-    user = User.create({username, password: hashed})
+  const payload = {
+    username,
+  };
+  const token = jwt.sign(payload, process.env.SECRET, { expiresIn: 120 });
+  await Token.create({ token, blackListed: false });
 
-    const payload = {username}
-    const token = jwt.sign(payload, process.env.SECRET, {expiresIn: 120});
+  res.set("Token", token);
 
-    res.set("Token", token);
-
-    return res.status(201).json({
-        msg: "All done",
-        token})
-})
+  return res.status(201).json({
+    message: "all done :)",
+    token,
+  });
+});
 
 app.post("/login", async (req, res) => {
-    const { username, password} = req.body;
-    const user = await User.findOne({ username });
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
 
-    if (!user) {
-        return res.status(404).json({error: "User not found"});
+  if (!user) {
+    return res.status(404).json({
+      error: "User not found",
+    });
+  }
+
+  const pswdMatch = bcrypt.compare(password, user.password);
+  if (!pswdMatch) {
+    return res.status(404).json({
+      error: "Invalid credentials",
+    });
+  }
+  const payload = { username };
+  const token = jwt.sign(payload, process.env.SECRET, { expiresIn: 120 });
+
+  await Token.create({ token, blackListed: false });
+
+  return res.status(201).json({
+    message: "Successfull login",
+    token,
+  });
+});
+
+app.get(
+  "/account",
+  expressjwt({ secret: process.env.SECRET, algorithms: ["HS256"] }),
+  (req, res) => {},
+);
+
+app.post(
+  "/logout",
+  expressjwt({ secret: process.env.SECRET, algorithms: ["HS256"] }),
+  async (req, res) => {
+    //req.headers.authorizations: Bearer fjaufhjuiorhfojfhjkdf
+    const token = req.headers.authorization.split(" ")[1];
+    const dbToken = await Token.findOne({ token }).exec();
+
+    if (dbToken.blackListed) {
+      return res.status(400).json({ err: "Invalid token!" });
     }
 
-    const pswdMatch = bcrypt.compare(password, user.password);
-    if(!pswdMatch) {
-        return res.status(404).json({error: "Invalid credentials"});
-    }
+    await Token.updateOne({ token }, { blackListed: true });
 
-    req.auth.username = username;
-
-    return res.status(201).json({msg: "Successful login :D"})
-
-})
-
-app.post("logout", expressjwt({ secret: process.env.SECRET, algorithms: ["HS256"]}),(req, res) => {
     res.status(204).json({
-        msg: "Successful Logout"
-    })
-})
+      message: "Successful Logout and Token Eliminate!",
+    });
+  },
+);
 
-app.listen(9000, () => console.log("HTTP Server running on port 9000"))
+app.listen(9000, () => console.log("Listening......."));
